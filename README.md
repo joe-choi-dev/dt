001378a1a3f16f63a844f85e1d6c8fd8571ea09a
joe-choi-dev <joe-choi-dev>
2023-10-17 11:48:15 -0700

    copy buttons

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Flow #
 
-0) `npm i` from both root and server and scripts
+0) `npm i` from root, server and scripts folders
 1) if new dt pdf run script to jsonify it `node dt-to-json.js` and copy it to `server/app.js`
 2) `npm run build` to generate the react build at root
-3) drag the build folder into the server
-4) `npm run start` from the /server folder to allow express
+3) drag the build folder into the server and kill the existing build
+4) `npm run start` from the /server folder to run express
 
-[Deployed Site](https://dt-today.onrender.com/)
-
-[Starter Code Source](https://www.youtube.com/watch?v=4pUBO31nkpk)
+[Deployed Site](https://dt-today.onrender.com/)  
+[Starter Code Source](https://www.youtube.com/watch?v=4pUBO31nkpk)  
--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [dt, setDt] = useState({});
-  const [scripture, setScripture] = useState("");
+  const [dt, setDt] = useState({}); // {"date":"Tuesday, October 17, 2023","text":"Galatians 4-6"}
+  const [esvResp, setEsvResp] = useState({}); // esv api body
+  const [esvDisplayText, setEsvDisplayText] = useState(""); // esv text to display
 
   useEffect(() => {
     fetch("/api/dt")
       .then((res) => res.json())
       .then((data) => setDt(data));
-  });
+  }, []);
 
   useEffect(() => {
     fetch(`/api/esv/${dt.text}`)
       .then((res) => res.json())
       .then((data) => {
-        const passages = data.passages[0].trim().split('[').filter(s => s !== "").join("<br />");
-        setScripture(passages)
+        setEsvResp(data);
+        (data.hasOwnProperty("passages") && Array.isArray(data.passages) && data.passages.length > 0) 
+          && setEsvDisplayText(data.passages[0].trim().split('[').filter(s => s !== "").join("<br />"));
       });
-  });
+  }, [dt]);
 
   function renderItems() {
     const esvUrl = `https://esv.org/${dt.text}`;
+
+    const lineByLine = (esvResp.hasOwnProperty("passages") && Array.isArray(esvResp.passages) && esvResp.passages.length > 0)  
+      ? esvResp.passages[0].trim().split('[').filter(s => s !== "").join("\r\n") 
+      : "";
+    const paragraph = (esvResp.hasOwnProperty("passages") && Array.isArray(esvResp.passages) && esvResp.passages.length > 0)  
+      ? esvResp.passages[0] 
+      : "";
     return (
       <div>
         <h3>Date - {dt.date}</h3>
         <h3><a href={esvUrl}>Text - {dt.text}</a></h3>
-        <p dangerouslySetInnerHTML={{__html: scripture}} />
+        {esvDisplayText !== "" ? (
+          <div>
+            <button onClick={() => {navigator.clipboard.writeText(lineByLine)}}>Copy Text - Line by Line</button>
+            <button onClick={() => {navigator.clipboard.writeText(paragraph)}}>Copy Text - Paragraph Format</button>
+            <p dangerouslySetInnerHTML={{__html: esvDisplayText}} />
+          </div>
+        ): <div/>}
       </div>
     );
   }
